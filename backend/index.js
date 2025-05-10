ce499ff9aee8654e5a90adf8ba8ef26cb7792888
Dakhani Usman <115381340+DakhaniUsman@users.noreply.github.com>
2025-05-10 15:27:25 +0530

    event nbooking system

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import AllRoutes from "./routes/index.js";
 import dotenv from "dotenv";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -10,19 +10,20 @@ app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-const connectDatabase = async() => {
-   await mongoose.connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB:", err);
-  });
-}
+const connectDatabase = async () => {
+  await mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Failed to connect to MongoDB:", err);
+    });
+};
 
-app.use("/api/v1/", AllRoutes)
+app.use("/api/v1/", AllRoutes);
 
-app.listen(8000, async() => {
-    await connectDatabase();
+app.listen(8000, async () => {
+  await connectDatabase();
   console.log("Server is running on port 8000");
 });
